cd0573c15dd3d04c8e644c29f86ed60eef1373eb
NikChao <nik.chaourov@gmail.com>
2018-08-05 13:47:29 +1000

    Add some lombok stuff as an example

--- src/main/java/com/example/demo/model/Widget.java
+++ src/main/java/com/example/demo/model/Widget.java
@@ -1,31 +1,23 @@
 package com.example.demo.model;
 
+import lombok.Getter;
+
 /**
  * Created by XTL on 8/14/2014.
  */
 public class Widget {
-
+    @Getter
     private String type;
 
+    @Getter
     private int length;
 
+    @Getter
     private int height;
 
     public Widget(String type, int length, int height) {
         this.type = type;
         this.length = length;
         this.height = height;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public int getLength() {
-        return length;
-    }
-
-    public int getHeight() {
-        return height;
     }
 }
